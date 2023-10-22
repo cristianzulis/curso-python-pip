d0736c6be51c0d745f7b3b244727fdfeaf448a71
cristianzulis <cristianzuluagalis@gmail.com>
2023-10-21 21:23:51 -0500

    generando esquema

--- my-movie-api/mainold.py
+++ my-movie-api/mainold.py
@@ -1,19 +1,9 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
-from typing import Optional
 
 app = FastAPI()
 app.title = "Example My API FASTAPI"
 app.version = "0.0.1"
-
-class Movie(BaseModel):
-    id: Optional[int] = None
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
 
 movies = [
     {
@@ -56,19 +46,26 @@ def get_movies_by_category(category:str, year:int):
     return [item for item in movies if item['category']==category]
 
 @app.post('/movies', tags=['movies'])
-def create_movie(movie:Movie):
-    movies.append(movie)
+def create_movie(id:int = Body(), title:str = Body(), overview:str = Body(), year:int = Body(), rating:float = Body(), category:str = Body()):
+    movies.append({
+        "id":id,
+        "title":title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category":category
+    })
     return movies
 
 @app.put('/movies/{id}', tags=['movies'])
-def update_movie(id:int, movie:Movie):
+def update_movie(id:int, title:str = Body(), overview:str = Body(), year:int = Body(), rating:float = Body(), category:str = Body()):
     for item in movies:
         if item["id"] == id:
-            item['title']= movie.title
-            item['overview']= movie.overview
-            item['year']= movie.year
-            item['rating']= movie.rating
-            item['category']= movie.category
+            item['title']=title
+            item['overview']=overview
+            item['year']=year
+            item['rating']=rating
+            item['category']=category
             return movies
 
 @app.delete('/movies/{id}', tags=['movies'])
